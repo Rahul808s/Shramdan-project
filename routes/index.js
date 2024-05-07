dcd345dc0779c3f5e6836b72caf3f4b2050c668f
RahulxDDDD <rahuldhote79123@gmail.com>
2024-05-08 00:06:48 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+  res.redirect('home', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+module.exports = router;
